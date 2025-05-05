8e6908de9dc3ec81a3b04692cc6f96c8b304de92
Витя <balezino2017@gmail.com>
2025-05-05 20:40:51 +0300

    Гонки

---
+++ 3.2.6_Races/src/Races.java
@@ -0,0 +1,19 @@
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.ActionListener;
+//import java.awt.image.BufferedImage;
+//
+//public class Races extends JPanel implements ActionListener {
+//    private static final int WIDTH = 800;
+//    private static final int HEIGHT = 800;
+//
+//    private Timer timer;
+//    private double speed;
+//    private BufferedImage carImage;
+//
+//    public Races() {
+//        try {
+//            BufferedImage Car1
+//        }
+//    }
+//}
